c2794b8d3afe635913c0170b15a621babf490b78
Bruno Borges <bborgesfilho33@gmail.com>
2023-07-19 14:03:55 -0300

    .

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -2,8 +2,8 @@ module.exports = (request, response, next) => {
   const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://mycontacts.up.railway.app/',
-    'https://mycontactsapi.up.railway.app/',
+    'https://mycontacts.up.railway.app',
+    'https://mycontactsapi.up.railway.app',
   ];
 
   const origin = request.header('Origin');
