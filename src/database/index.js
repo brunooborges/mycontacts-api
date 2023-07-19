955eee4180c2129e7157a86a3ab546ab6432a5fe
Bruno Borges <bborgesfilho33@gmail.com>
2023-07-19 10:00:00 -0300

    updating DB

--- src/database/index.js
+++ src/database/index.js
@@ -4,11 +4,11 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
 const client = new Client({
-  host: 'babar.db.elephantsql.com',
-  port: 5432,
+  host: 'containers-us-west-139.railway.app',
+  port: 6108,
   user: dbUser,
   password: dbPassword,
-  database: 'nyoabbhg',
+  database: 'railway',
 });
 
 client.connect();
