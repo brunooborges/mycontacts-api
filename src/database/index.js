08d4ba3d4babe3b6c81ea2dc58b442a11cf2312e
Bruno Borges <bborgesfilho33@gmail.com>
2023-07-18 22:40:49 -0300

    updating SSL

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,17 @@
 const { Client } = require('pg');
 
+const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
 const client = new Client({
   host: 'babar.db.elephantsql.com',
   port: 5432,
-  user: 'nyoabbhg',
+  user: dbUser,
   password: dbPassword,
   database: 'nyoabbhg',
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
